0de88c6bfd14bef6a616f225ebf1b937beeda9d0
Vaibhav Goswami <vaibhavgoswami404@gmail.com>
2023-06-01 10:36:35 +0530

    added core

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connectToMongo()
 
 app.use(express.json())
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://inotebook-vaibhav.onrender.com/')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', '*')
     res.header('Access-Control-Allow-Headers', '*')
     next()
